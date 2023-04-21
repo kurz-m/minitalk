5af1bd4f967d7c828bc1217fdc5529796264ea69
kurz-m <m.kurz92@gmail.com>
2023-04-21 21:26:32 +0200

    added server.c

--- server.c
+++ server.c
@@ -6,11 +6,20 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 08:45:43 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/21 16:50:29 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/21 20:01:22 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	acknowledge_reception(int acknowledge)
+{
+	char	received_char;
+	if (acknowledge == SIGUSR2)
+		received_char = ' ';
+	else
+		received_char = '1';
+}
 
 void	listen_for_client(pid_t pid)
 
@@ -24,6 +33,7 @@ int	main(void)
 		exit(EXIT_FAILURE);
 	pid = getpid();
 	ft_putnbr_fd(pid, 1);
+
 	
 	return (EXIT_SUCCESS);
 }
