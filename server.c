fedabfa50991a4816b897012657e9267087c9c33
Markus Kurz <makurz@2-E-1.42heilbronn.de>
2023-04-21 16:50:35 +0200

    Savestate.

--- server.c
+++ server.c
@@ -6,11 +6,13 @@
 /*   By: makurz <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/21 08:45:43 by makurz            #+#    #+#             */
-/*   Updated: 2023/04/21 16:49:25 by makurz           ###   ########.fr       */
+/*   Updated: 2023/04/21 16:50:29 by makurz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minitalk.h"
+
+void	listen_for_client(pid_t pid)
 
 int	main(void)
 {
